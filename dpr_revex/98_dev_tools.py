e7994c9e2d1ee3fad9866e1bd02365bcce5431ea
Joseph Tricarico <joseph.tricarico@delaware.gov>
2026-02-21 00:16:02 +0000

    add pull to git commit code block in dev tools

--- dpr_revex/98_dev_tools.py
+++ dpr_revex/98_dev_tools.py
@@ -14,7 +14,7 @@
 # %% [markdown] id="8lP7mci0vqPa"
 # # Dev Tools
 
-# %% colab={"base_uri": "https://localhost:8080/"} id="4Mna7DKmvSgF" executionInfo={"status": "ok", "timestamp": 1771620060407, "user_tz": 300, "elapsed": 1631, "user": {"displayName": "Joseph Tricarico", "userId": "06693078329233897993"}} outputId="0dd873f3-19c0-46fc-ec9a-12e92118e772"
+# %% colab={"base_uri": "https://localhost:8080/"} id="4Mna7DKmvSgF" executionInfo={"status": "ok", "timestamp": 1771632334999, "user_tz": 300, "elapsed": 22904, "user": {"displayName": "Joseph Tricarico", "userId": "06693078329233897993"}} outputId="18424070-f200-4775-90df-81c11e964202"
 import os, subprocess, sys
 from pathlib import Path
 from google.colab import drive, userdata
@@ -27,7 +27,7 @@ REPO = 'joetric/dnrec-dpr-revex-engine'
 GITHUB_TOKEN = userdata.get('GITHUB_TOKEN')
 
 
-# %% colab={"base_uri": "https://localhost:8080/"} id="noACbGrpv5XK" executionInfo={"status": "ok", "timestamp": 1771620157452, "user_tz": 300, "elapsed": 821, "user": {"displayName": "Joseph Tricarico", "userId": "06693078329233897993"}} outputId="b2096747-9073-4ee8-a477-6b582331702e"
+# %% id="noACbGrpv5XK" executionInfo={"status": "ok", "timestamp": 1771632335036, "user_tz": 300, "elapsed": 30, "user": {"displayName": "Joseph Tricarico", "userId": "06693078329233897993"}}
 def clone_repo():
     token = userdata.get('GITHUB_TOKEN') # get GitHub token for colab; repo and content scoped
     result = subprocess.run(
@@ -56,7 +56,7 @@ def init_repo():
 # %% [markdown] id="FXd2d8XSvnkv"
 # ## Convert Jupyter notebooks to .py
 
-# %% colab={"base_uri": "https://localhost:8080/"} id="f9QtbH1Ru16o" executionInfo={"status": "ok", "timestamp": 1771620758680, "user_tz": 300, "elapsed": 9327, "user": {"displayName": "Joseph Tricarico", "userId": "06693078329233897993"}} outputId="98b3fe27-ac35-4fb2-b710-596ff8652055"
+# %% colab={"base_uri": "https://localhost:8080/"} id="f9QtbH1Ru16o" executionInfo={"status": "ok", "timestamp": 1771632346145, "user_tz": 300, "elapsed": 11106, "user": {"displayName": "Joseph Tricarico", "userId": "06693078329233897993"}} outputId="790502c1-a07e-4a73-e0b2-5e84de34a8b2"
 import subprocess
 
 nb_dir = PROJ / 'dpr_revex'
@@ -67,10 +67,14 @@ for nb in nb_dir.glob('*.ipynb'):
 # %% [markdown] id="kN7nPAEN0nyQ"
 # ## Git commit
 
-# %% colab={"base_uri": "https://localhost:8080/"} id="bgHuOEu5vt74" executionInfo={"status": "ok", "timestamp": 1771621286120, "user_tz": 300, "elapsed": 5091, "user": {"displayName": "Joseph Tricarico", "userId": "06693078329233897993"}} outputId="069dc6c3-ff3b-41cc-b3ac-5940161b95c7"
+# %% colab={"base_uri": "https://localhost:8080/"} id="bgHuOEu5vt74" executionInfo={"status": "ok", "timestamp": 1771632392040, "user_tz": 300, "elapsed": 11229, "user": {"displayName": "Joseph Tricarico", "userId": "06693078329233897993"}} outputId="d51909b6-fbcb-4386-c27c-e95975636624"
 msg = input('Commit message: ')
-# # !git config --global user.email "your.name@delaware.gov"
-# # !git config --global user.name "Firstname Lastname"
+# !git config --global user.email "joseph.tricarico@delaware.gov"
+# !git config --global user.name "Joseph Tricarico"
 # !git -C {PROJ} add *.py *.yaml *.toml .gitignore
 # !git -C {PROJ} commit -m "{msg}"
 # !git -C {PROJ} push origin main
+
+# %% colab={"base_uri": "https://localhost:8080/"} id="x7_IHIhkhTnc" executionInfo={"status": "ok", "timestamp": 1771632443027, "user_tz": 300, "elapsed": 2136, "user": {"displayName": "Joseph Tricarico", "userId": "06693078329233897993"}} outputId="12b69fea-49e6-43d6-ab57-0fb7633352ef"
+# !git -C {PROJ} fetch
+# !git -C {PROJ} pull
