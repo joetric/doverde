65bfd41093675209bf731504ac99f1961feebeaf
Joseph Tricarico <joseph.tricarico@delaware.gov>
2026-02-21 00:16:24 +0000

    rm scratch code block

--- dpr_revex/98_dev_tools.py
+++ dpr_revex/98_dev_tools.py
@@ -56,7 +56,7 @@ def init_repo():
 # %% [markdown] id="FXd2d8XSvnkv"
 # ## Convert Jupyter notebooks to .py
 
-# %% colab={"base_uri": "https://localhost:8080/"} id="f9QtbH1Ru16o" executionInfo={"status": "ok", "timestamp": 1771632346145, "user_tz": 300, "elapsed": 11106, "user": {"displayName": "Joseph Tricarico", "userId": "06693078329233897993"}} outputId="790502c1-a07e-4a73-e0b2-5e84de34a8b2"
+# %% colab={"base_uri": "https://localhost:8080/"} id="f9QtbH1Ru16o" outputId="a16e9842-b87a-46a7-dcfa-f817d0f05b45"
 import subprocess
 
 nb_dir = PROJ / 'dpr_revex'
@@ -67,14 +67,15 @@ for nb in nb_dir.glob('*.ipynb'):
 # %% [markdown] id="kN7nPAEN0nyQ"
 # ## Git commit
 
-# %% colab={"base_uri": "https://localhost:8080/"} id="bgHuOEu5vt74" executionInfo={"status": "ok", "timestamp": 1771632392040, "user_tz": 300, "elapsed": 11229, "user": {"displayName": "Joseph Tricarico", "userId": "06693078329233897993"}} outputId="d51909b6-fbcb-4386-c27c-e95975636624"
+# %% id="bgHuOEu5vt74"
 msg = input('Commit message: ')
 # !git config --global user.email "joseph.tricarico@delaware.gov"
 # !git config --global user.name "Joseph Tricarico"
+# !git -C {PROJ} pull origin main
 # !git -C {PROJ} add *.py *.yaml *.toml .gitignore
 # !git -C {PROJ} commit -m "{msg}"
 # !git -C {PROJ} push origin main
 
-# %% colab={"base_uri": "https://localhost:8080/"} id="x7_IHIhkhTnc" executionInfo={"status": "ok", "timestamp": 1771632443027, "user_tz": 300, "elapsed": 2136, "user": {"displayName": "Joseph Tricarico", "userId": "06693078329233897993"}} outputId="12b69fea-49e6-43d6-ab57-0fb7633352ef"
+# %% id="x7_IHIhkhTnc"
 # !git -C {PROJ} fetch
 # !git -C {PROJ} pull
